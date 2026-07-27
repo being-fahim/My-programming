8add310fc0df93a73e9f7caefeffdd704a58bd2b
Md Razzak Hossain Fahim <262-16-115@diu.edu.bd>
2026-07-27 11:52:39 +0600

    Update TheAnnoyingMathQuiz.c
    You can give two number and select an operator, the solver will get 3 times to input the right answer.

--- TheAnnoyingMathQuiz.c
+++ TheAnnoyingMathQuiz.c
@@ -1,18 +1,52 @@
 #include <stdio.h>
 
 int main(){
+
     int a,b,c,e;
-
-    printf("input two number: ");
-    scanf("%d %d", &a, &b);
-    e = a+b;
+    char f;
+        printf("Enter the value of A: ");
+        scanf("%d",&a);
+        printf("Enter the value of B: ");
+        scanf("%d", &b);
+        printf("Select an operator: ");
+        scanf(" %c", &f);
+    int count = 0;
     do{
-        printf("what the answer of %d + %d: ", a, b);
-        scanf("%d", &c);
+            switch(f){
+            case '+':
+                e = a + b;
+                printf("What is the value of %d + %d = ",a,b);
+                scanf("%d", &c);
+                break;
+            case '-':
+                e = a - b;
+                printf("What is the value of %d - %d = ",a,b);
+                scanf("%d", &c);
+                break;
+            case '*':
+                e = a * b;
+                printf("What is the value of %d X %d = ",a,b);
+                scanf("%d", &c);
+                break;
+            case '/':
+                e = a / b;
+                printf("What is the value of %d / %d = ",a,b);
+                scanf("%d", &c);
+                break;
+            }
+            if(c != e){
+                if(count < 2){
+                    printf("\nWrong Answer!\nTry again.\n\n");
+                }
+                count++;
+            }
+    }while(c != e && count != 3);
 
-        if(c != e){
-            printf("Wrong answer! Type again.\n");
-        }
-    }while(c != e);
-    printf("Right answer.");
+            if(c == e){
+                printf("\n             Congradulations!\n");
+            }else{
+                printf("\n          Better luck next time!");
+                printf("\n");
+            }
+
 }
